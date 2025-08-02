0efd7a3e426f0c9dce9506101dd1c666754ef38d
Enzo Almeida <enzo.alverde@gmail.com>
2025-08-02 19:57:49 -0300

    fix: corrigindo api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,12 @@
 // src/services/api.ts
 import axios from 'axios';
 
+const baseURL = 'https://bacelar-api.onrender.com';
+
+console.log(`[API Service] Conectando à base: ${baseURL}`);
+
 const api = axios.create({
-  baseURL: 'https://bacelar-api.onrender.com/api/v1',
+  baseURL: `${baseURL}/api/v1`,
 });
 
 api.interceptors.request.use(async (config) => {
@@ -13,4 +17,5 @@ api.interceptors.request.use(async (config) => {
   return config;
 });
 
+// Simplificamos de volta, o interceptor não vai atrapalhar o login
 export default api;
